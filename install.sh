0060497b456bc6462eb223c4da3d5f662cea04d0
immanuel-peter <immanuel.peter1@gmail.com>
2026-06-05 02:58:07 -0500

    Run Hostess installer with bash

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-curl -fsSL https://hostess.sh/install.sh | sh
+curl -fsSL https://hostess.sh/install.sh | bash
 
 if ! command -v hostess >/dev/null 2>&1; then
   if [ -x "$HOME/.local/bin/hostess" ]; then
